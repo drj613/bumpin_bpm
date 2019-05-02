8331f0cc3e09690b627c812ba05f81b770252dcf
drj613 <drj613@gmail.com>
2019-05-01 18:42:53 -0700

    found songs, added some notes to JS

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
 window.onload = function(){
 
-    
+    let currentlyPlaying = {
+        buttonId: "",
+        songUrl: ""
+    };
+
+    //onclick event for button element
+    //push clicked button id into currentlyPlaying.buttonId
+    //stop currentlyPlaying
+    //push new song into currentlyPlaying.songUrl
+
+
+    //do mathses for background pulse
 }
